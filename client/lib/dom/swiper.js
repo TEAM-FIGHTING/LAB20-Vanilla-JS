81e2fa340df86f2cc57d726b6764c4875e02a5c5
kimmoonju-102 <kimmun3549@naver.com>
2023-01-31 15:26:13 +0900

    [feat] 메인베너 마크업 추가 #5

---
+++ client/lib/dom/swiper.js
@@ -0,0 +1,25 @@
+import { getNode } from "./getNode.js";
+
+export function swiperWrapper() {
+  const swiper = new Swiper('.swiper', {
+    // Optional parameters
+    direction: 'vertical',
+    loop: true,
+
+    // If we need pagination
+    pagination: {
+      el: '.swiper-pagination',
+    },
+
+    // Navigation arrows
+    navigation: {
+      nextEl: '.swiper-button-next',
+      prevEl: '.swiper-button-prev',
+    },
+
+    // And if we need scrollbar
+    scrollbar: {
+      el: '.swiper-scrollbar',
+    },
+  });
+}
