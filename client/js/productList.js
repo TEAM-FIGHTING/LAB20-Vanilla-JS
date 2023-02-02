f8d9e3122d9cf71f5216430df4a13accc6082cc7
Sumin Kim <ksm6399@gmail.com>
2023-02-03 01:47:24 +0900

    [style] Cart pop-up 수정 #21

--- client/js/productList.js
+++ client/js/productList.js
@@ -1,56 +1,50 @@
+import { getNode, getNodes } from "../lib/index.js";
+import { insertLast } from "../lib/dom/insert.js";
 
-// import { getNode, getNodes } from '../lib/index.js';
-import { insertLast } from '../lib/dom/insert.js';
-import { getNode, getNodes } from './../lib/dom/getNode.js';
+const body = getNode("body");
+const [...buttons] = getNodes(".accordion");
+const [...carts] = getNodes(".cart-icon");
+const background = getNode(".background");
+const cancelButton = getNode(".add-cart__button > button");
 
-const body = getNode('body')
-const [...buttons] = getNodes('.accordion');
-const [...carts] = getNodes('.cart-icon');
-const back = getNode('.back');
-const cancelButton = getNode('.add-cart__button > button')
-
-function handler(e){
+function handler(e) {
   let dv = e.currentTarget;
-  let ch = dv.children[1]
-  let arrowIconImg = getNode('.arrow-icon > img');
+  let ch = dv.children[1];
+  let arrowIconImg = getNode(".arrow-icon > img");
 
-  if(ch.style.display=='none'){ 		
-    ch.style.display = 'block'; 	
-    arrowIconImg.src = './assets/productList/Direction=Up.png'
-  }else{ 		
-    ch.style.display = 'none'; 	
-    arrowIconImg.src = './assets/productList/Direction=Down.png'
-  } 
+  if (ch.style.display == "none") {
+    ch.style.display = "block";
+    arrowIconImg.src = "./assets/productList/Direction=Up.png";
+  } else {
+    ch.style.display = "none";
+    arrowIconImg.src = "./assets/productList/Direction=Down.png";
+  }
 }
 
-for (let button of buttons){
-  button.addEventListener('click', handler)
+for (let button of buttons) {
+  button.addEventListener("click", handler);
 }
 
-
-function cartHandler(e){
-
-  back.style.display = 'block';
+function cartHandler(e) {
+  background.style.display = "block";
   let scroll = scrollY;
-  console.log(scroll);
-  body.style.overflow = 'scroll';
-  body.style.position =  'fixed';
-  body.style.top = `${-scroll}px`
-  body.style.width = '100%';
-  body.style.height = '100%'
-  
+  body.style.overflow = "scroll";
+  body.style.position = "fixed";
+  body.style.top = `${-scroll}px`;
+  body.style.width = "100%";
+  body.style.height = "100%";
 }
 
-for (let cart of carts){
-  cart.addEventListener('click', cartHandler)
+for (let cart of carts) {
+  cart.addEventListener("click", cartHandler);
 }
-
-
 
 function cancelButtonHandler() {
-  back.style.display = 'none';
-  body.style.overflow = 'visible';
-  body.style.position =  'static';
+  background.style.display = "none";
+  body.style.removeProperty("overflow");
+  body.style.removeProperty("position");
+  body.style.removeProperty("top");
+  body.style.removeProperty("width");
+  body.style.removeProperty("height");
 }
-cancelButton.addEventListener('click', cancelButtonHandler)
-
+cancelButton.addEventListener("click", cancelButtonHandler);
