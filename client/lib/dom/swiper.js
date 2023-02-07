c7587963b0001f42ac840b5ee572faf7e6dd8dc7
kimmoonju-102 <kimmun3549@naver.com>
2023-02-07 09:28:32 +0900

    [feat] 메인 페이지에서 swiper 기능 구현

--- client/lib/dom/swiper.js
+++ client/lib/dom/swiper.js
@@ -1,25 +1,60 @@
-import { getNode } from "./getNode.js";
+const swiper1 = new Swiper('.swiper-1', {
+  // Optional parameters
+  loop: true,
 
-export function swiperWrapper() {
-  const swiper = new Swiper('.swiper', {
-    // Optional parameters
-    direction: 'vertical',
-    loop: true,
+  // If we need pagination
+  pagination: {
+    el: '.swiper-1__pagination',
+    type: "fraction",
+  },
 
-    // If we need pagination
-    pagination: {
-      el: '.swiper-pagination',
-    },
+  // Navigation arrows
+  navigation: {
+    nextEl: '.main_banner-button-next',
+    prevEl: '.main_banner-button-prev',
+  },
+});
 
-    // Navigation arrows
-    navigation: {
-      nextEl: '.swiper-button-next',
-      prevEl: '.swiper-button-prev',
-    },
 
-    // And if we need scrollbar
-    scrollbar: {
-      el: '.swiper-scrollbar',
-    },
-  });
-}
+const swiper2 = new Swiper('.swiper-2', {
+  // Optional parameters
+  slidesPerView: 4,
+  spaceBetween: 18,
+  loop: true,
+  loopFillGroupWithBlank: true,
+	slidesPerGroup: 4,
+  // Navigation arrows
+  navigation: {
+    prevEl: '.product-button-prev1',
+    nextEl: '.product-button-next2',
+  },
+});
+
+const swiper3 = new Swiper('.swiper-3', {
+  // Optional parameters
+  slidesPerView: 4,
+  spaceBetween: 18,
+  loop: true,
+  loopFillGroupWithBlank: true,
+	slidesPerGroup: 4,
+  // Navigation arrows
+  navigation: {
+    prevEl: '.product2-button-prev3',
+    nextEl: '.product2-button-next4',
+  },
+});
+
+const swiper4 = new Swiper('.swiper-4', {
+  // Optional parameters
+  direction: 'vertical',
+  spaceBetween: 8,
+  loop: true,
+  loopFillGroupWithBlank: true,
+  slidesPerGroup: 1,
+  // Navigation arrows
+  navigation: {
+    prevEl: '.side-nav__bottom-button',
+    nextEl: '.side-nav__top-button',
+  },
+
+});
