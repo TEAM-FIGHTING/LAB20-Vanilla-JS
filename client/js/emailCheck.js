9f76180d4971bd281ac4f2edf475e038daf31cc1
lulla-by <thfl3363@hanmail.net>
2023-02-07 11:37:23 +0900

    [feat] 회원가입 중복체크 공백검사 추가 #43

--- client/js/emailCheck.js
+++ client/js/emailCheck.js
@@ -8,11 +8,25 @@ async function validation(e) {
   let data = await tiger.get("http://localhost:3000/users");
   let user = data.data.find((user) => user.email === emailValue.value);
 
-  if (user) {
-    console.log("이미 사용중인 이메일 입니다.");
+  // if (user) {
+  //   console.log("이미 사용중인 이메일 입니다.");
+  // } else {
+  //   console.log("사용 가능한 이메일 입니다.");
+  // }
+
+  if (emailValue.value.trim() !== "") {
+    if (user) {
+      alert("이미 사용중인 이메일 입니다.");
+    } else {
+      alert("사용 가능한 이메일 입니다.");
+    }
   } else {
-    console.log("사용 가능한 이메일 입니다.");
+    alert("공백은 사용불가능한 이메일 입니다.");
   }
+
+
+
+
 }
 
 checkButton.addEventListener("click", validation);
--- client/js/idCheck.js
+++ client/js/idCheck.js
@@ -6,13 +6,24 @@ const idValue = getNode("#id");
 async function validation() {
   event.preventDefault();
   let data = await tiger.get("http://localhost:3000/users");
-  let user = data.data.find((user) => user.username === idValue.value);
+  console.log(data.data)
+  let user = data.data.find((user) => user.id === idValue.value);
 
-  if (user) {
-    console.log("이미 사용중인 아이디 입니다.");
+
+  if (idValue.value.trim() !== "") {
+    if (user) {
+      alert("이미 사용중인 아이디 입니다.");
+    } else {
+      alert("사용 가능한 아이디 입니다.");
+    }
   } else {
-    console.log("사용 가능한 아이디 입니다.");
+    alert("공백은 사용불가능한 아이디입니다.");
   }
 }
+  
+
+
+
+
 
 checkButton.addEventListener("click", validation);
