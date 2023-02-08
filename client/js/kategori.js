28bedb43fa6f87dc0f4b1f9dd79c322ac81d0fad
BrightSton <gusehf321@naver.com>
2023-02-08 09:06:08 +0900

    [feat] 스크롤 위치에 따른 헤더 요약 기능 구현 #15

--- client/js/kategori.js
+++ client/js/kategori.js
@@ -2,6 +2,9 @@ import { getNode } from "../lib/index.js";
 
 const kategori = getNode(".kategorie__item__name");
 const kategoriMenu = getNode(".kategorie__item__menu");
+const kategoriSummary = getNode("#kategorie__item__name--summary");
+const kategoriMenuSummary = getNode("#kategorie__item__menu--summary");
+const navigation = getNode(".navigation--summary");
 
 function handleKategoriDown() {
   if (kategoriMenu.style.display === "block") {
@@ -11,5 +14,31 @@ function handleKategoriDown() {
   }
 }
 
+function handleKategoriSummaryDown() {
+  if (kategoriMenuSummary.style.display === "block") {
+    kategoriMenuSummary.style.display = "none";
+  } else {
+    kategoriMenuSummary.style.display = "block";
+  }
+}
+
+function handleNavigationOn() {
+  if (window.scrollY > 200) {
+    navigation.style.display = "block";
+  } else {
+    navigation.style.display = "none";
+  }
+}
+
+function handlekategoriMenuSummaryOn() {
+  if (window.scrollY < 750) {
+    kategoriMenuSummary.style.display = "none";
+  }
+}
+
 kategori.addEventListener("mouseover", handleKategoriDown);
 kategoriMenu.addEventListener("mouseleave", handleKategoriDown);
+window.addEventListener("scroll", handleNavigationOn);
+kategoriSummary.addEventListener("mouseover", handleKategoriSummaryDown);
+kategoriMenuSummary.addEventListener("mouseleave", handleKategoriSummaryDown);
+window.addEventListener("scroll", handlekategoriMenuSummaryOn);
